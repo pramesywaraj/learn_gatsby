8d8fe60d65a8cc06e0f0b81a385dca0d760eca61
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2019-12-10 17:28:25 +0700

    start new one base on the tutorial && add several pages

---
+++ src/pages/blogs.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function BlogPage() {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>Ini adalah contoh post</p>
+        </div>
+    )
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ContactPage() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Email: jembarpanalar21@gmail.com</p>
+        </div>
+    )
+}
